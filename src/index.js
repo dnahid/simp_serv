affa9a38ba5ddbc012d02960d32971bbd98f2aa0
Nahidul Islam <nahidul.islam.cs@gmail.com>
2020-04-27 16:05:56 +0300

    Change directory structure.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("got to /demo for getting you are alive me");
+});
+
+app.get("/demo", (req, res) => {
+  res.send("I'm alive!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
